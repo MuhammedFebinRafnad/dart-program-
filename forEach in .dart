db2bbcf24f2c54668cf680c7ada0bbf0339b85f6
MuhammedFebinRafnad <muhammedfebinrafnadth@gmail.com>
2025-01-01 22:24:51 +0530

    Update forEach in .dart

--- forEach in .dart
+++ forEach in .dart
@@ -1,9 +1,7 @@
 // void main(){
 //   List<int>numb=[1,2,3,4,5,6,7,8,9,10];
 //  numb.forEach((itvalue)=>print(itvalue));
-  
 //   }
- 
 void main(){
   List<int>list1=[1,2,3,4,5,6,7,8,9,10];
   int sum=0;
