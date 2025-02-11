3085da8ed97681929388f54259797a1561fceae1
MuhammedFebinRafnad <muhammedfebinrafnadth@gmail.com>
2025-02-11 22:17:38 +0530

    Update forloop in list.dart

--- forloop in list.dart
+++ forloop in list.dart
@@ -5,13 +5,13 @@
 //   }
 // }
 void main(){
-  List<int>numb1=[1,2,3,4,5,];
+  List<int>numb1=[1,2,3,4,5,6,7];
   int sum=0;
   for(int i=0;i<=numb1.length;i++){
     sum=sum+i;
   }
   print(sum);
-  List<int>numb2=[3,5,2,6,8,4,1];
+  List<int>numb2=[3,5,2,6,8,4,1,7,9];
   numb2.sort();
   print(numb2);
   numb2.shuffle();
